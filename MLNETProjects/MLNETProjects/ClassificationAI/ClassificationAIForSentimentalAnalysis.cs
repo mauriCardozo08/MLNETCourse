929747307eb35ccb6aa85aa7380d432c2df700dc
Mauricio Cardozo <mauricio.cardozo08@gmail.com>
2025-09-27 12:30:37 -0300

    feat: add ClassificationAI and RegressionAI

---
+++ MLNETProjects/MLNETProjects/ClassificationAI/ClassificationAIForSentimentalAnalysis.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MlNETProjects.ClassificationAI
+{
+    public class ClassificationAIForSentimentalAnalysis
+    {
+        public ClassificationAIForSentimentalAnalysis() { }
+
+        public void Execute()
+        {
+            //TrainTheModel();
+            TestModel();
+        }
+
+        #region Testing the model
+
+        private void TestModel() 
+        {
+            string basePath = "C:\\Users\\mcardozo\\source\\repos\\MlNETProjects\\MlNETProjects\\ClassificationAI";
+            string modelPath = Path.Combine(basePath, "model.zip");
+            string testDataPath = Path.Combine(basePath, "test.csv");
+
+            MLContext mLContext = new MLContext();
+            ITransformer model;
+            using(var stream = new FileStream(modelPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                model = mLContext.Model.Load(stream, out var modelInputSchema);
+            }
+
+            IDataView testData = mLContext.Data.LoadFromTextFile<TextData>(testDataPath, hasHeader: true, separatorChar: ',');
+
+            var predictor = mLContext.Model.CreatePredictionEngine<TextData, SentimentPrediction>(model);
+            var testDataList = mLContext.Data.CreateEnumerable<TextData>(testData, reuseRowObject: false).ToList();
+
+            foreach (var data in testDataList) 
+            {
+                var prediction = predictor.Predict(data);
+                Console.WriteLine($"Text:{data.text} | Positive Sentiment{prediction.IsPositiveSentiment}");
+            }
+        }
+    
+
+        public class TextData 
+        {
+            [LoadColumn(0)]
+            public string text { get; set; }
+        }
+
+        public class SentimentPrediction 
+        {
+            [ColumnName("Score")]
+            public float SentimentScore { get; set; }
+
+            public bool IsPositiveSentiment => SentimentScore < 0.5f;
+        }
+
+        #endregion
+
+
+        #region Trainning the model
+        private void TrainTheModel() 
+        {
+            MLContext mLContext = new MLContext();
+            string dataPath = "C:\\Users\\mcardozo\\source\\repos\\MlNETProjects\\MlNETProjects\\ClassificationAI\\train.csv";
+            string text = File.ReadAllText(dataPath);
+
+            using (StreamReader sr = new StreamReader(dataPath))
+            {
+                text = text.Replace("\'", "");
+            }
+            File.WriteAllText(dataPath, text);
+
+            IDataView dataView = mLContext.Data.LoadFromTextFile<MovieReview>(dataPath, hasHeader: true, allowQuoting: true, separatorChar: ',');
+
+            var pipeline = mLContext.Transforms.Text.FeaturizeText("Features", "text")
+                .Append(mLContext.BinaryClassification.Trainers.SdcaLogisticRegression("Label", "Features"));
+
+            var model = pipeline.Fit(dataView);
+            var predictions = model.Transform(dataView);
+            var metrics = mLContext.BinaryClassification.Evaluate(predictions, "Label");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy}");
+            Console.WriteLine($"Precision: {metrics.PositivePrecision}");
+            Console.WriteLine($"Recall: {metrics.PositiveRecall}");
+            Console.WriteLine($"F1Score: {metrics.F1Score}");
+            mLContext.Model.Save(model, dataView.Schema, "C:\\Users\\mcardozo\\source\\repos\\MlNETProjects\\MlNETProjects\\ClassificationAI\\model.zip");
+        }
+
+        public class MovieReview 
+        {
+            [LoadColumn(0)]
+            public string text { get; set; }
+
+            [LoadColumn(1)]
+            [ColumnName("Label")]
+            public bool sentiment { get; set; }
+        }
+
+        #endregion
+    }
+}
---
+++ MLNETProjects/MLNETProjects/RegressionAI/RegressionAI.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace MlNETProjects.RegressionAI
+{
+    public class RegressionAI
+    {
+        public RegressionAI() 
+        {
+
+        }
+
+        public void Execute()
+        {
+            TrainAndTestTheModel();
+        }
+
+        private void TrainAndTestTheModel()
+        {
+            var mlContext = new MLContext(seed: 0);
+            var dataPath = "C:\\Users\\mcardozo\\source\\repos\\MlNETProjects\\MlNETProjects\\RegressionAI\\house-price-data.csv";
+
+            IDataView dataView = mlContext.Data.LoadFromTextFile<HouseData>(dataPath, separatorChar: ',', hasHeader: true);
+            var trainTestData = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            var trainData = trainTestData.TrainSet;
+            var testData = trainTestData.TestSet;
+
+            var pipeline = mlContext.Transforms.Concatenate("Features", "HouseSize", "NumberOfBedrooms", "NumberOfBathrooms")
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding("Neighborhood"))
+                .Append(mlContext.Transforms.Concatenate("Features", "Features", "Neighborhood")
+                .Append(mlContext.Transforms.CopyColumns("Label", "Price"))
+                .Append(mlContext.Regression.Trainers.FastTree(labelColumnName: "Label")));
+
+            var trainedModel = pipeline.Fit(trainData);
+
+            var predictions = trainedModel.Transform(testData);
+            var metrics = mlContext.Regression.Evaluate(predictions);
+
+            Console.WriteLine($"R^2: {metrics.RSquared:0.##}");
+            Console.WriteLine($"RMS: {metrics.RootMeanSquaredError:#.##}");
+
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<HouseData, HousePricePrediction>(trainedModel);
+
+            var sampleHouse = new HouseData()
+            {
+                HouseSize = 2500f,
+                NumberOfBedrooms = 4f,
+                NumberOfBathrooms = 3f,
+                Neighborhood = "Northeast"
+            };
+            var pricePrediction = predictionEngine.Predict(sampleHouse);
+            Console.WriteLine($"Predicted price for the house: {pricePrediction.PredictedSalePrice:C}");
+        }
+    }
+
+    public class HouseData 
+    {
+        [LoadColumn(0)]
+        public float HouseSize { get; set; }
+
+        [LoadColumn(1)]
+        public float NumberOfBedrooms { get; set; }
+
+        [LoadColumn(2)]
+        public float NumberOfBathrooms { get; set; }
+
+        [LoadColumn(3)]
+        public string Neighborhood { get; set; }
+
+        [LoadColumn(4)]
+        public float Price { get; set; }
+    }
+
+    public class HousePricePrediction 
+    {
+        [ColumnName("Score")]
+        public float PredictedSalePrice { get; set; }
+    }
+}
